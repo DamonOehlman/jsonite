367ec745de2462adcd4a042133b92d3d6c1ec147
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-30 21:16:27 +1000

    generate uuid values for id

--- store.js
+++ store.js
@@ -3,38 +3,27 @@ var levelup = require('levelup');
 var sublevel = require('level-sublevel');
 var path = require('path');
 var extend = require('xtend');
+var uuid = require('uuid');
 
 module.exports = function(opts) {
   var defaultdb = path.resolve((opts || {}).cwd || process.cwd(), 'jsonite-data');
   var db = sublevel(levelup((opts || {}).db || defaultdb));
   var counters = db.sublevel('_counters');
 
-  function nextId(type, callback) {
-    counters.get(type, function(err, value) {
-      value = (parseInt(value, 10) || 0) + 1;
-
-      counters.put(type, value, function(err) {
-        callback(err, value);
-      });
-    });
-  }
-
   function substore(type) {
     return db.sublevel(type, { valueEncoding: 'json' });
   }
 
   db.create = function(type, data, callback) {
-    nextId(type, function(err, id) {
-      if (err) {
-        return callback(err);
-      }
-
-      // inject the id into the data
-      data = extend({}, data, { id: id });
-      substore(type).put(id, data, function(err) {
-        callback(err, data);
-      });
+    debug('attempting to create new ' + type);
+    data = extend({ id: uuid.v4() }, data);
+    substore(type).put(id, data, function(err) {
+      callback(err, data);
     });
+  };
+
+  db.get = function(type, id, callback) {
+    substore(type).get(id, callback);
   };
 
   return db;
