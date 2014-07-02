30965469b4fc11b2b7ae71bc7b1b1ef2fc1013e6
Damon Oehlman <damon.oehlman@gmail.com>
2014-07-02 16:32:05 +1000

    Define a person resource

--- test/define-resources.js
+++ test/define-resources.js
@@ -1,7 +1,24 @@
 var Joi = require('joi');
 
 module.exports = function(api, test) {
-  test('define a resource', function(t) {
+  test('define a person', function(t) {
+    t.plan(2);
+
+    var schema = api.provide('person', {
+      name: api.types.string().required()
+    });
+
+    // validating an incorrect object fails
+    Joi.validate({}, schema, function(err, value) {
+      t.ok(err, 'captured validation error');
+    });
+
+    Joi.validate({ name: 'Dave Lister' }, schema, function(err, value) {
+      t.ifError(err, 'validation ok');
+    });
+  });
+
+  test('define a post', function(t) {
     t.plan(2);
 
     var schema = api.provide('post', {
